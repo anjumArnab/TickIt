1e3018d90c40694f21199df263893179b6a26bbb
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-11-06 18:52:13 +0600

    data models for hive

---
+++ lib/models/hive/pomodoro_session.dart
@@ -0,0 +1,85 @@
+import 'package:hive/hive.dart';
+
+part 'pomodoro_session.g.dart';
+
+@HiveType(typeId: 2)
+class PomodoroSession extends HiveObject {
+  @HiveField(0)
+  DateTime startTime;
+
+  @HiveField(1)
+  DateTime endTime;
+
+  @HiveField(2)
+  SessionType sessionType;
+
+  @HiveField(3)
+  int plannedDuration; // in seconds
+
+  @HiveField(4)
+  int actualDuration; // in seconds
+
+  @HiveField(5)
+  bool completed;
+
+  PomodoroSession({
+    required this.startTime,
+    required this.endTime,
+    required this.sessionType,
+    required this.plannedDuration,
+    required this.actualDuration,
+    required this.completed,
+  });
+
+  @override
+  String toString() {
+    return 'PomodoroSession(startTime: $startTime, endTime: $endTime, sessionType: $sessionType, completed: $completed)';
+  }
+}
+
+@HiveType(typeId: 3)
+enum SessionType {
+  @HiveField(0)
+  work,
+
+  @HiveField(1)
+  shortBreak,
+
+  @HiveField(2)
+  longBreak,
+}
+
+// models/pomodoro_settings.dart
+@HiveType(typeId: 4)
+class PomodoroSettings extends HiveObject {
+  @HiveField(0)
+  int workDuration; // in seconds
+
+  @HiveField(1)
+  int shortBreakDuration; // in seconds
+
+  @HiveField(2)
+  int longBreakDuration; // in seconds
+
+  @HiveField(3)
+  int sessionsUntilLongBreak;
+
+  @HiveField(4)
+  bool autoStartBreaks;
+
+  @HiveField(5)
+  bool autoStartWorkSessions;
+
+  @HiveField(6)
+  bool soundEnabled;
+
+  PomodoroSettings({
+    this.workDuration = 25 * 60,
+    this.shortBreakDuration = 5 * 60,
+    this.longBreakDuration = 15 * 60,
+    this.sessionsUntilLongBreak = 4,
+    this.autoStartBreaks = false,
+    this.autoStartWorkSessions = false,
+    this.soundEnabled = true,
+  });
+}
---
+++ lib/models/hive/task.g.dart
@@ -0,0 +1,90 @@
+part of 'task.dart';
+
+class TaskAdapter extends TypeAdapter<Task> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Task read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Task(
+      title: fields[0] as String,
+      time: fields[1] as String,
+      isMainTaskCompleted: fields[2] as bool,
+      flagColorValue: fields[3] as int,
+      subtasks: (fields[4] as List).cast<Subtask>(),
+      date: fields[5] as DateTime,
+      workspace: fields[6] as String?,
+      workspaceColorValue: fields[7] as int?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Task obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.time)
+      ..writeByte(2)
+      ..write(obj.isMainTaskCompleted)
+      ..writeByte(3)
+      ..write(obj.flagColorValue)
+      ..writeByte(4)
+      ..write(obj.subtasks)
+      ..writeByte(5)
+      ..write(obj.date)
+      ..writeByte(6)
+      ..write(obj.workspace)
+      ..writeByte(7)
+      ..write(obj.workspaceColorValue);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class SubtaskAdapter extends TypeAdapter<Subtask> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Subtask read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Subtask(title: fields[0] as String, isCompleted: fields[1] as bool);
+  }
+
+  @override
+  void write(BinaryWriter writer, Subtask obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.isCompleted);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SubtaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
